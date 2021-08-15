ab314f41b8778bd2c556ccc8597b9b592ce612e4
Bachicada <bachicada@gmail.com>
2021-08-16 00:08:30 +0300

    docs:done morse-decoder

--- src/index.js
+++ src/index.js
@@ -38,8 +38,33 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
-}
+    expr=expr.replace(/(.{10})/g,"$1 ");
+    let arr=[];
+    arr =expr.split(' ')
+    let copyArr=[];
+    for (let j = 0; j < arr.length; j++) {
+      copyArr.push(arr[j].replace(/^0+(?!\.)/,""))
+    }
+    
+    let arrDots=[];
+    for (let j = 0; j < copyArr.length; j++) {
+      arrDots.push(copyArr[j].replace(/10/g,"."))
+    }
+    let arrDashes=[];
+    for (let j = 0; j < arrDots.length; j++) {
+      arrDashes.push(arrDots[j].replace(/11/g,"-"))
+    }
+  
+    let arrLetters=[];
+    arrLetters = arrDashes.map(function(char) {
+    return MORSE_TABLE[char];})
+
+   let arrSpace=[];
+   // for (let j = 0; j < arrDots.length; j++) {
+     arrSpace = arrLetters.map(function(item){return item == undefined? '+' : item;});
+    
+    return arrSpace.join('').replace(/\+/g,' ').trimEnd();
+    }
 
 module.exports = {
     decode
